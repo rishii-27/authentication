611e49cde5c4010f763c8dcc4aced6a2cedccef4
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2023-11-04 18:51:07 +0530

    Authentication request on SignUp

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,30 +1,82 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandle = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if (isLogin) {
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCCp_PNWJPafnJpmzekxyWnglUwipx9mrs",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          // On Registration successful
+          emailInputRef.current.value = "";
+          passwordInputRef.current.value = "";
+        } else {
+          return res.json().then((data) => {
+            // Show error modal
+            console.log(data);
+          });
+        }
+      });
+    }
+  };
+
   return (
     <div className="container mt-5">
       <div className="row justify-content-center">
         <div className="col-sm-10 col-md-8 col-lg-6">
-          <div className="card p-4"> {/* Add card class for a container with padding */}
-            <h1 className="text-center mb-4">{isLogin ? "Login" : "Sign Up"}</h1>
-            <form>
+          <div className="card p-4">
+            <h1 className="text-center mb-4">
+              {isLogin ? "Login" : "Sign Up"}
+            </h1>
+            <form onSubmit={submitHandle}>
               <div className="mb-3">
                 <label htmlFor="email" className="form-label">
                   Your Email
                 </label>
-                <input type="email" id="email" className="form-control" required />
+                <input
+                  type="email"
+                  id="email"
+                  className="form-control"
+                  ref={emailInputRef}
+                  required
+                />
               </div>
               <div className="mb-3">
                 <label htmlFor="password" className="form-label">
                   Your Password
                 </label>
-                <input type="password" id="password" className="form-control" required />
+                <input
+                  type="password"
+                  id="password"
+                  className="form-control"
+                  ref={passwordInputRef}
+                  required
+                />
               </div>
               <div className="d-grid">
                 <button className="btn btn-primary">
